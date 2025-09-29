487621b1a3641021f80eb95686fcddd1dc6456d6
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-29 17:07:04 +0900

    DoubleBuffer
    DoubleBuffer

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,83 @@
 #include <iostream>
 #include <conio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <Windows.h>
 
 using namespace std;
 
-int Player[2] = { 0, 0 };
-char PlayerShape = 'P';
+// C++ -> class
+// UE
+struct FCharacter
+{
+	int X;
+	int Y;
+	string Shape;
+};
 
-int Monster[2] = { 0, 0 };
-char MonsterShape = 'M';
+//typedef :: struct _COORD를 COORD로 호출한다(C언어 문법)
+//typedef struct _COORD {
+//	SHORT X;
+//	SHORT Y;
+//} COORD;
+
+FCharacter Characters[3];
 
 char KeyCode;
 bool bIsContinue = true;
 
 int MapSize = 0;
 
+HANDLE ScreenBuffers[2];
 
-bool Predict(int Coord)
+enum ERenderScreenBuffer
 {
-	if (Coord < 0)
+	FrontBuffer = 0,
+	BackBuffer = 1,
+};
+
+int CurrentBufferIndex = FrontBuffer;
+
+void Init()
+{
+	srand((unsigned int)time(nullptr));
+
+	//0, nullptr, NULL 다 같은 의미
+	ScreenBuffers[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, nullptr, CONSOLE_TEXTMODE_BUFFER, NULL);
+	ScreenBuffers[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, nullptr, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+	cout << "맵의 크기를 입력해 주세요(NxN) : ";
+	cin >> MapSize;
+	system("cls");
+
+	/*FCharacter* Data = new FCharacter;
+
+	delete Data;*/
+
+	Characters[0].X = 0;
+	Characters[0].Y = 0;
+	Characters[0].Shape = "P";
+
+	Characters[1].X = rand() % MapSize;
+	Characters[1].Y = rand() % MapSize;
+	Characters[1].Shape = "M";
+
+	Characters[2].X = rand() % MapSize;
+	Characters[2].Y = rand() % MapSize;
+	Characters[2].Shape = "B";
+}
+
+void Input()
+{
+	KeyCode = _getch();
+}
+
+
+bool Predict(int Coordinate)
+{
+	if (Coordinate < 0)
 	{
 		return false;
 	}
-	else if (Coord >= MapSize)
+	else if (Coordinate >= MapSize)
 	{
 		return false;
 	}
@@ -33,34 +87,34 @@ bool Predict(int Coord)
 	}
 }
 
-void Process()
+void MovePlayer()
 {
 	if (KeyCode == 'w')
 	{
-		if (Predict(Player[1] - 1))
+		if (Predict(Characters[0].Y - 1))
 		{
-			Player[1]--;
+			Characters[0].Y--;
 		}
 	}
 	else if (KeyCode == 's')
 	{
-		if (Predict(Player[1] + 1))
+		if (Predict(Characters[0].Y + 1))
 		{
-			Player[1]++;
+			Characters[0].Y++;
 		}
 	}
 	else if (KeyCode == 'a')
 	{
-		if (Predict(Player[0] - 1))
+		if (Predict(Characters[0].X - 1))
 		{
-			Player[0]--;
+			Characters[0].X--;
 		}
 	}
 	else if (KeyCode == 'd')
 	{
-		if (Predict(Player[0] + 1))
+		if (Predict(Characters[0].X + 1))
 		{
-			Player[0]++;
+			Characters[0].X++;
 		}
 	}
 	else if (KeyCode == 'q')
@@ -69,51 +123,111 @@ void Process()
 	}
 }
 
-void Render()
+void MoveMonster(int Index)
 {
-	for (int Y = 0; Y < MapSize; Y++)
+	int Direction = rand() % 4;
+	switch (Direction)
 	{
-		for (int X = 0; X < MapSize; X++)
+	case 0: //Up
+		if (Predict(Characters[Index].Y - 1))
 		{
-			if (Y == Player[1] && X == Player[0])
-			{
-				cout << PlayerShape;
-			}
-			else if (Y == Monster[1] && X == Monster[0])
-			{
-				cout << MonsterShape;
-			}
-			else
-			{
-				cout << " ";
-			}
+			Characters[Index].Y--;
 		}
-		cout << "\n";
+		break;
+	case 1://Down
+		if (Predict(Characters[Index].Y + 1))
+		{
+			Characters[Index].Y++;
+		}
+		break;
+	case 2://Left
+		if (Predict(Characters[Index].X - 1))
+		{
+			Characters[Index].X--;
+		}
+		break;
+	case 3://Right
+		if (Predict(Characters[Index].X + 1))
+		{
+			Characters[Index].X++;
+		}
+		break;
+	default:
+		//Error
+		break;
 	}
 }
 
-int main()
+void Tick()
 {
-	srand((unsigned int)time(nullptr));
+	MovePlayer();
+	for (int i = 1; i < 3; i++)
+	{
+		MoveMonster(i);
+	}
+}
 
-	cout << "맵의 크기를 입력해 주세요(NxN) : ";
-	cin >> MapSize;
-	system("cls");
+void Clear()
+{
+	COORD coordScreen = { 0, 0 };
+	DWORD cCharsWritten;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD dwConSize;
 
-	//최초 렌딩 1회
-	cout << PlayerShape;
+	//스크린 버퍼 정보 가져오기
+	GetConsoleScreenBufferInfo(ScreenBuffers[CurrentBufferIndex], &csbi);
+	dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+	FillConsoleOutputCharacter(ScreenBuffers[CurrentBufferIndex],
+		(TCHAR)' ',
+		dwConSize,
+		coordScreen,
+		&cCharsWritten);
+}
 
-	Monster[0] = rand() % MapSize;
-	Monster[1] = rand() % MapSize;
+//call by reference
+void RenderCharacter(const FCharacter* Character)
+{
+	COORD Position;
+	Position.X = (SHORT)Character->X;
+	Position.Y = (SHORT)Character->Y;
+
+	//Character->Y++; //const 선언한 데이터는 수정할 경우 오류뜸
+
+	SetConsoleCursorPosition(ScreenBuffers[CurrentBufferIndex], Position);
+	WriteConsole(ScreenBuffers[CurrentBufferIndex], Character->Shape.c_str(), 1, NULL, NULL);
+}
+
+void Present()
+{
+	SetConsoleActiveScreenBuffer(ScreenBuffers[CurrentBufferIndex]);
+
+	CurrentBufferIndex++;
+	CurrentBufferIndex %= 2;
+}
+
+void Render()
+{
+	//그래픽 카드 그리는 방식
+	Clear();
+
+	//RenderAll
+	for (int i = 2; i >= 0; i--)
+	{
+		RenderCharacter(&Characters[i]);
+	}
+
+	Present();
+}	
+
+int main(int argc, char* argv[])
+{
+	Init();
 
 	while (bIsContinue)
 	{
-		KeyCode = _getch();
-
-		system("cls");
-
-		Process();
 		Render();
+		Input();
+		Tick();
 	}
 
 	return 0;
