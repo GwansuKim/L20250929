a89ff08b7f83aea52a5b4ff5e85bf05f9afac41d
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-29 09:45:40 +0900

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int Player[2] = { 0, 0 };
+char PlayerShape = 'P';
+
+int Monster[2] = { 0,0 };
+char MonsterShape = 'M';
+
+char KeyCode;
+bool bIsContinue = true;
+
+int MapSize = 0;
+
+
+bool Predict(int Player)
+{
+	if (Player < 0)
+	{
+		return false;
+	}
+	else if (Player > MapSize)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+void Process()
+{
+	if (KeyCode == 'w')
+	{
+		if (Predict(Player[1] - 1))
+		{
+			Player[1]--;
+		}
+	}
+	else if (KeyCode == 's')
+	{
+		if (Predict(Player[1] + 1))
+		{
+			Player[1]++;
+		}
+	}
+	else if (KeyCode == 'a')
+	{
+		if (Predict(Player[0] - 1))
+		{
+			Player[0]--;
+		}
+	}
+	else if (KeyCode == 'd')
+	{
+		if (Predict(Player[0] + 1))
+		{
+			Player[0]++;
+		}
+	}
+	else if (KeyCode == 'q')
+	{
+		bIsContinue = false;
+	}
+}
+
+void Render()
+{
+	for (int Y = 0; Y <= MapSize; Y++)
+	{
+		for (int X = 0; X <= MapSize; X++)
+		{
+			if (Y == Player[1] && X == Player[0])
+			{
+				cout << PlayerShape;
+			}
+			else if (Y == Monster[1] && X == Monster[0])
+			{
+				cout << MonsterShape;
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int main()
+{
+	srand((unsigned int)time(nullptr));
+
+	cout << "맵의 크기를 입력해 주세요(NxN) : ";
+	cin >> MapSize;
+	system("cls");
+
+	//최초 렌딩 1회
+	cout << PlayerShape;
+
+	Monster[0] = rand() % MapSize;
+	Monster[1] = rand() % MapSize;
+
+	while (bIsContinue)
+	{
+		KeyCode = _getch();
+
+		system("cls");
+
+		Process();
+		Render();
+	}
+
+	return 0;
+}
